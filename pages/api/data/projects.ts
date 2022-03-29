249fd8397ecad3b4f807f72cd23ad861541e16a6
jakoblistabarth <jakob@listabarth.at>
2022-03-29 23:04:33 +0200

    :label: Add missing types for modules

--- pages/api/data/projects.ts
+++ pages/api/data/projects.ts
@@ -1,9 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import getProjects from "../../../lib/getProjects";
 
+export type Project = {
+  project_id: string;
+  projectName: string;
+  projectShortName: string;
+  countries: string[];
+  type: string;
+  status: string;
+};
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Project[]>
 ) {
   res.status(200).json(await getProjects());
 }
