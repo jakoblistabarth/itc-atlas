4dd0138cce9501a5d69fe12ca0b0c76c6160de0b
jakoblistabarth <jakob@listabarth.at>
2022-03-29 17:04:55 +0200

    :card_file_box: Join ISO codes to countries topojson

--- lib/getCountries.ts
+++ lib/getCountries.ts
@@ -1,5 +1,18 @@
+import getUnsdCodes from "./getUnsdCodes";
+
 export default async function getCountries(scale: string) {
   const url = `https://cdn.jsdelivr.net/npm/world-atlas@2/countries-${scale}.json`;
-  const json = await fetch(url).then((res) => res.json());
-  return json;
+  const world = await fetch(url).then((res) => res.json());
+  const unsdCodes = await getUnsdCodes("countries");
+
+  // TODO: fails for "W. Sahara", "Dem. Rep. Congo", "Fr. S. Antarctic Lands", "Côte d'Ivoire", "Eq. Guinea", "eSwatini", "Laos", "Vietnam", "North Korea", "South Korea", "Taiwan", "N. Cyprus", "Somaliland", "Kosovo", "S. Sudan"
+  world.objects.countries.geometries.forEach((d) => {
+    const matchedCountry = unsdCodes.find((code) =>
+      code["Country or Area"].match(d.properties.name)
+    );
+    if (!matchedCountry) return;
+    d.properties.iso3code = matchedCountry["ISO-alpha3 Code"];
+  });
+
+  return world;
 }
