e63907ccbb56b709fef3bb948dd4287ddcf409b6
jakoblistabarth <jakob@listabarth.at>
2022-03-30 13:11:24 +0200

    :alembic: Use types for value mapping

--- lib/mappings/project.type.ts
+++ lib/mappings/project.type.ts
@@ -1,4 +1,14 @@
-export default {
+export type ProjectType =
+  | "Education and Training"
+  | "Consulting"
+  | "Contract Education"
+  | "Contract Research"
+  | "Other"
+  | "Research";
+
+type TypeMapping = { [key: string]: ProjectType };
+
+const mappedTypes: TypeMapping = {
   "Interne Reseach": "Research",
   Integrated: "Other",
   "Decentralization of Education": "Education and Training",
@@ -6,3 +16,5 @@ export default {
   "Seminar/Workshop/Symposium": "Education and Training",
   "10, 21 and 61": "Other",
 };
+
+export default mappedTypes;
