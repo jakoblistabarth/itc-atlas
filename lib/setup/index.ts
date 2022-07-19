76a886b6845eb8b12b1ac1bac0379dd8a0493ffd
jakoblistabarth <jakob@listabarth.at>
2022-07-19 13:49:12 +0200

    :bug: Make data setup async
    * Only call fakeFlightsData once the airports are downloaded

--- lib/setup/index.ts
+++ lib/setup/index.ts
@@ -1,7 +1,8 @@
-import fakeTravelData from "./fakeTravelData";
 import loadNaturalEarthData from "./loadNaturalEarthData";
 import loadAirports from "./loadAirports";
+import fakeTravelData from "./fakeTravelData";
 
-loadAirports();
-fakeTravelData();
 loadNaturalEarthData();
+loadAirports().then(() => {
+  fakeTravelData();
+});
