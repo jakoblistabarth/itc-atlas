763cba369966bdda6e73b2d294a0a50e777dbdb4
jakoblistabarth <jakob@listabarth.at>
2022-09-05 07:59:18 +0200

    :memo: Add documentation to lonLatToXYZ

--- lib/cartographic/lonLatToXYZ.ts
+++ lib/cartographic/lonLatToXYZ.ts
@@ -1,11 +1,15 @@
 import { Vector3 } from "three";
 
-export const lonLatToXYZ = (
-  lon: number,
-  lat: number,
-  radius: number,
-  offset = 0
-) => {
+/**
+ * Returns the Cartesian coordinates of a point given as pair of geographic coordinates,
+ * providing the radius of a sphere and an optional offset to this sphere.
+ * @param lon The coordinate pair's longitude coordinate.
+ * @param lat The coordinate pair's latitude coordinate.
+ * @param radius The radius of the sphere used for the conversion
+ * @param offset An offset (from the surface of this sphere) used to
+ * @returns A {@link Vector3} holding the Cartesian coordinates of the point.
+ */
+const lonLatToXYZ = (lon: number, lat: number, radius: number, offset = 0) => {
   const phi = (lat * Math.PI) / 180;
   const theta = ((lon - 180) * Math.PI) / 180;
 
