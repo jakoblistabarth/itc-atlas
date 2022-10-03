89fe1da6d5fc8b0f93595f423d8b958360c31a22
jakoblistabarth <jakob@listabarth.at>
2022-10-03 06:21:53 +0200

    :lipstick: Fix layout of proportional symbol legend

--- components/map/ProportionalSymbolLegend.tsx
+++ components/map/ProportionalSymbolLegend.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import * as d3 from "d3";
-import { ScalePower } from "d3";
+import { scaleLinear, ScalePower } from "d3";
 import { fInt } from "../../lib/utilities/formaters";
 import { Appearance } from "../../types/Appearance";
 import LegendTitle from "./LegendTitle";
@@ -19,7 +19,13 @@ const ProportionalSymbolLegend: FC<{
   const max = d3.max(data);
   const median = d3.median(data);
   const mean = d3.mean(data);
-  if (!min || !max || !median || !mean) return <g />;
+  if (
+    min == undefined ||
+    max == undefined ||
+    median == undefined ||
+    mean == undefined
+  )
+    return <g />;
   const entries = [min, median, Math.ceil(mean), max];
 
   const labelOffset = style?.text?.fontSize ?? 10;
@@ -29,46 +35,81 @@ const ProportionalSymbolLegend: FC<{
     .x((d) => d[0])
     .y((d) => d[1]);
 
+  const diameter = scaleRadius(max) * 2;
+  const xScale = scaleLinear()
+    .domain([0, max - min])
+    .range([0, diameter]);
+
   return (
     <g id="legend" transform={`translate(${x}, ${y})`}>
       <LegendTitle>{title}</LegendTitle>
-      <g id="legendEntries" transform="translate(1, 70)">
-        {entries.map((entry) => {
-          const maxRadius = scaleRadius(max);
-          const entryRadius = scaleRadius(entry);
-          const suffix = entry > 1 ? "s" : "";
-          const label = fInt(entry) + " " + unitLabel + suffix;
-          const linePath = line([
-            [maxRadius, maxRadius - 2 * entryRadius],
-            [maxRadius * 2, maxRadius - 2 * entryRadius],
-          ]);
-          if (!linePath) return <></>;
-          return (
-            <g key={nanoid()}>
-              <circle
-                cx={maxRadius}
-                cy={maxRadius - entryRadius}
-                r={entryRadius}
-                fill={style?.fill ?? "none"}
-                fillOpacity={style?.fillOpacity}
-                stroke={style?.stroke ?? "black"}
-                strokeWidth={style?.strokeWidth ?? "1"}
-              />
-              <path
-                stroke={style?.stroke ?? "black"}
-                strokeWidth={0.5}
-                d={linePath}
-              />
-              <text
-                fontSize={10}
-                x={maxRadius * 2 + labelOffset}
-                y={maxRadius - entryRadius * 2} //TODO: prevent overlaps
-              >
-                {typeof entry === "number" ? label : null}
-              </text>
-            </g>
-          );
-        })}
+      <g transform={`translate(1, ${20 + 20 + diameter / 2})`}>
+        <g id="legendEntries">
+          {entries.map((entry) => {
+            const maxRadius = scaleRadius(max);
+            const entryRadius = scaleRadius(entry);
+            const suffix = entry > 1 ? "s" : "";
+            const label = fInt(entry) + " " + unitLabel + suffix;
+            const linePath = line([
+              [maxRadius, maxRadius - 2 * entryRadius],
+              [maxRadius * 2, maxRadius - 2 * entryRadius],
+            ]);
+            if (!linePath) return <></>;
+            return (
+              <g key={nanoid()}>
+                <circle
+                  cx={maxRadius}
+                  cy={maxRadius - entryRadius}
+                  r={entryRadius}
+                  fill={style?.fill ?? "none"}
+                  fillOpacity={style?.fillOpacity}
+                  stroke={style?.stroke ?? "black"}
+                  strokeWidth={style?.strokeWidth ?? "1"}
+                />
+                <path
+                  stroke={style?.stroke ?? "black"}
+                  strokeWidth={0.5}
+                  d={linePath}
+                />
+                <text
+                  fontSize={10}
+                  x={maxRadius * 2 + labelOffset}
+                  y={maxRadius - entryRadius * 2} //TODO: prevent overlaps
+                >
+                  {typeof entry === "number" ? label : null}
+                </text>
+              </g>
+            );
+          })}
+        </g>
+        <g transform={`translate(0,${diameter / 2 + 20})`}>
+          <rect
+            x={0}
+            y={0}
+            width={xScale(max - min)}
+            height={scaleRadius(max)}
+            stroke="grey"
+            fill="none"
+            strokeWidth={0.5}
+          />
+          <g>
+            {d3.range(min, max).map((val, idx) => {
+              const y = scaleRadius(val);
+              const x = xScale(idx);
+              return (
+                <circle key={nanoid()} cx={x} cy={scaleRadius(max) - y} r={1} />
+              );
+            })}
+          </g>
+          <line
+            x1="0"
+            x2={xScale(max - min)}
+            y1={scaleRadius(max)}
+            y2={scaleRadius(max)}
+            strokeWidth={1}
+            stroke={"black"}
+          />
+        </g>
       </g>
     </g>
   );
---
+++ stories/ProportionalSymbolLegend.stories.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { scaleSqrt } from "d3";
+
+import ProportionalSymbolLegend from "../components/map/ProportionalSymbolLegend";
+
+const width = 600;
+const height = 300;
+
+const testData = [
+  0.25, 3, 6, 10, 4, 5, 30, 2, 100, 110, 30.8, 80, 200, 120, 197,
+];
+
+export default {
+  title: "Cartographic/Legend/ProportionalSymbolLegend",
+  argTypes: {
+    min: {
+      control: { type: "range", min: 0, max: 100, step: 1 },
+    },
+  },
+  component: ProportionalSymbolLegend,
+  decorators: [
+    (Story) => (
+      <svg width={width} height={height}>
+        <Story />
+      </svg>
+    ),
+  ],
+} as ComponentMeta<typeof ProportionalSymbolLegend>;
+
+const Template: ComponentStory<typeof ProportionalSymbolLegend> = (args) => {
+  return (
+    <>
+      <ProportionalSymbolLegend {...args} />
+    </>
+  );
+};
+
+const defaultArgs = {
+  x: 0,
+  y: 0,
+  data: testData,
+  unitLabel: "fruit",
+  scaleRadius: scaleSqrt().domain([0, 200]).range([5, 30]),
+  title: "Apples per tree",
+};
+
+export const simpleLegend = Template.bind({});
+simpleLegend.args = {
+  ...defaultArgs,
+};
