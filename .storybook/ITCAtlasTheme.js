ea17fb56de051e275a33096aa8b2290b4fc01954
jakoblistabarth <jakob@listabarth.at>
2022-12-05 16:44:05 +0100

    :memo: Add basic custom theme for storybook

---
+++ .storybook/ITCAtlasTheme.js
@@ -0,0 +1,12 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+
+  fontBase: '"Inter", serif',
+
+  brandTitle: "ITC Atlas Component Library",
+  //   brandUrl: "https://example.com",
+  //   brandImage: "https://place-hold.it/350x150",
+  brandTarget: "_self",
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/addons";
+import ITCAtlasTheme from "./ITCAtlasTheme";
+
+addons.setConfig({
+  theme: ITCAtlasTheme,
+});
