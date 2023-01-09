23943b0c3bd8aec79caa0d50275d96667edb083e
jakoblistabarth <jakob@listabarth.at>
2023-01-09 10:11:16 +0100

    :card_file_box: Use semicolon as delimiter for unsd data

--- lib/data/getUnsdCountries.ts
+++ lib/data/getUnsdCountries.ts
@@ -4,6 +4,8 @@ import { AreaCode } from "../../types/UnsdCodes";
 export default async function getUnsdCountries() {
   const csvFilePath = "./data/static/UNSD-Methodology.csv";
   // source: https://unstats.un.org/unsd/methodology/m49/overview/
-  const countries: AreaCode[] = await csv().fromFile(csvFilePath);
+  const countries: AreaCode[] = await csv({ delimiter: ";" }).fromFile(
+    csvFilePath
+  );
   return countries;
 }
