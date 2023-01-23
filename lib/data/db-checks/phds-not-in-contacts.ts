620288436b29df3170898a2a33462be728bca6aa
jakoblistabarth <jakob@listabarth.at>
2023-01-23 06:45:49 +0100

    :monocle_face: Add data checks related to db relations

---
+++ lib/data/db-checks/phds-not-in-contacts.ts
@@ -0,0 +1,21 @@
+import getContacts from "../getContacts";
+import * as aq from "arquero";
+import getPhdCandidates from "../getPhdCandidates";
+import { PhdCandidate } from "../../../types/PhdCandidate";
+
+(async () => {
+  const contactData = await getContacts();
+  const phdData = await getPhdCandidates();
+
+  const tb = aq.from(contactData).dedupe("itcStudentId").select("itcStudentId");
+  const res = tb.objects() as { itcStudentId?: string }[];
+  const contactIds = res.map((d) => d.itcStudentId).filter((d) => d);
+
+  const missing = phdData.reduce((acc: PhdCandidate[], d) => {
+    if (!d.itcStudentId) return acc;
+    if (!contactIds.includes(d.itcStudentId)) acc.push(d);
+    return acc;
+  }, []);
+  // phd candidates by itcStudentId which do not have a match by itcStudentId in contact table
+  console.log(missing.map((d) => d.itcStudentId));
+})();
