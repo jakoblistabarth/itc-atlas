4ca6a616def534e722e576c23f09e46738dbb873
jakoblistabarth <jakob@listabarth.at>
2023-02-05 12:23:10 +0100

    :recycle: Exclude js files from stories pattern

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+  stories: ["../stories/**/*.stories.@(ts|tsx|mdx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
