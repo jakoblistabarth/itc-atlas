5f735cf26ed6998bc81d37011ee7dd6a825dd8d9
jakoblistabarth <jakob@listabarth.at>
2023-02-14 17:26:34 +0100

    :coffin: Remove unused function to filter Travels

--- lib/data/getTravelsByCountry.ts
+++
@@ -1,9 +0,0 @@
-import { Travel } from "../../types/Travels";
-import getTravels from "./getTravels";
-
-const getTravelsByCountry = async (countryName: string): Promise<Travel[]> => {
-  const travels = await getTravels();
-  return travels.filter((travel) => travel.country === countryName);
-};
-
-export default getTravelsByCountry;
