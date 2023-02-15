5226d7734246497c70bacb3b0624f07b8272883e
jakoblistabarth <jakob@listabarth.at>
2023-02-15 06:42:06 +0100

    :coffin: Remove script to fake travelData

--- lib/setup/index.ts
+++ lib/setup/index.ts
@@ -1,12 +1,7 @@
 import loadNaturalEarthData from "./loadNaturalEarthData";
 import loadAirports from "./loadAirports";
-import fakeTravelData from "./fakeTravelData";
-import loadCities from "./loadCitiesOver1000Population";
 import createCourseGenealogy from "./createCourseGenealogy";
 
 createCourseGenealogy();
 loadNaturalEarthData();
-loadAirports().then(() => {
-  fakeTravelData();
-});
-// loadCities();
+loadAirports();
