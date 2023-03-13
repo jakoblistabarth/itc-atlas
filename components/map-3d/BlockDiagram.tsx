783e6bc92fab8759dabbab0058a40f329671d4e6
jakoblistabarth <jakob@listabarth.at>
2023-03-13 11:53:07 +0100

    :construction: Add shader tests

---
+++ components/map-3d/BlockDiagram.tsx
@@ -0,0 +1,71 @@
+import { FC, useEffect, useMemo, useRef } from "react";
+import { BufferAttribute, Mesh, PlaneGeometry, ShaderMaterial } from "three";
+import { useFrame } from "@react-three/fiber";
+
+type Props = {
+  side: number;
+  segments: number;
+  data: Float32Array;
+};
+
+// TODO: refactor with react drei fiber's shaderMaterial for declarative uniforms: https://docs.pmnd.rs/react-three-fiber/tutorials/typescript#extend-usage
+// for typing, see: https://docs.pmnd.rs/react-three-fiber/tutorials/typescript#extend-usage
+
+// TODO: if shaders get more complex move shaders to separate files: https://github.com/glslify/glslify-loader
+
+const BlockDiagramm: FC<Props> = ({ side, segments, data }) => {
+  const vertexShader = /*glsl*/ `
+  attribute float displacement;
+
+  void main() {
+    vec3 newPosition = position + vec3(0,0,displacement);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+
+  }
+  `;
+
+  // TODO: remove random color (introduced for debugging)
+  const fragmentShader = /*glsl*/ `
+  void main() {
+    gl_FragColor = vec4(0.0, ${Math.random()}, ${Math.random()}, 1.0);
+  }
+  `;
+
+  const meshRef = useRef<Mesh<PlaneGeometry, ShaderMaterial>>(null);
+  const geomRef = useRef<PlaneGeometry>(null);
+
+  useEffect(() => {
+    geomRef.current?.setAttribute("displacement", new BufferAttribute(data, 1));
+  });
+
+  const uniforms = useMemo(
+    () => ({
+      u_time: {
+        value: 0.0,
+      },
+    }),
+    []
+  );
+
+  useFrame((state) => {
+    if (!meshRef.current) return;
+    const { clock } = state;
+    meshRef.current.material.uniforms.u_time.value = clock.getElapsedTime();
+  });
+
+  return (
+    <>
+      <mesh ref={meshRef} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry ref={geomRef} args={[side, side, segments, segments]} />
+        <shaderMaterial
+          vertexShader={vertexShader}
+          fragmentShader={fragmentShader}
+          uniforms={uniforms}
+          wireframe
+        />
+      </mesh>
+    </>
+  );
+};
+
+export default BlockDiagramm;
