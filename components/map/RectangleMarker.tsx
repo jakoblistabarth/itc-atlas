e5aa52eab3e91fbf2777effe91d03028a682e4b4
Pei Nie <niepei@usc.edu.cn>
2023-06-06 22:41:55 +0800

    :sparkles:Create RectangleHighlightComponent

---
+++ components/map/RectangleMarker.tsx
@@ -0,0 +1,24 @@
+import { FC, SVGProps } from "react";
+import { Vector2 } from "three";
+import { Text } from "@visx/text";
+
+type Props = {
+  position: Vector2;
+  width?: number;
+  height?: number;
+} & SVGProps<SVGCircleElement>;
+
+const RectangleMarker: FC<Props> = ({
+  position,
+  width,
+  height,
+  ...rest
+}) => {
+  return (
+    <g transform={`translate(${position.x} ${position.y})`}>
+      <rect  width={width} height={height} fill='lightgrey' strokeWidth='1' stroke='black' />;
+    </g>
+  );
+};
+
+export default RectangleMarker;
