80697e15227af560266ffa523cbfb6fa37b15466
Pei Nie <niepei@usc.edu.cn>
2023-06-06 22:39:21 +0800

    :sparkles:Create endpoints of  LocatorMap

--- pages/api/maps/locatormap.tsx
+++ pages/api/maps/locatormap.tsx
@@ -2,22 +2,33 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import ReactDOMServer from "react-dom/server";
 import LocatorMap from "../../../components/map/LocatorMap";
 import getCountries from "../../../lib/data/getCountries";
-
+import RectangleMarker from "../../../components/map/RectangleMarker";
+import RoundMarker from "../../../components/map/RoundMarker";
+import { Vector2 } from "three";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const highlightCountries = req.query.country?.toString();
+  const lat=req.query.lat?.toString();
+  const lng=req.query.lng?.toString();
   if (!highlightCountries) {
     res.status(400).json({ error: "invalid iso-3-code name" });
   }
+  //const marker=[32.723084,86.933545];
+  const marker=[Number(lat),Number(lng)];
+  var position:number[][]=[marker];
   var highlights:string[]=[highlightCountries??""];
   const neCountriesTopoJson = getCountries();
-  
+  const width=25;
+  const height=10;
   const svg = ReactDOMServer.renderToStaticMarkup(
     <LocatorMap 
     neCountriesTopoJson={neCountriesTopoJson}
     highlight={highlights}
+    rectwidth={width}
+    rectheight={height}
+    rectanglehighlight={position}
     ></LocatorMap>
   );
 
