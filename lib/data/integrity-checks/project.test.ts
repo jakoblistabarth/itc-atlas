94665f4e60f6d9bc58197c7afd670ab7720baeb2
jakoblistabarth <jakob@listabarth.at>
2023-06-07 16:27:20 +0200

    :card_file_box: Fix secondary department import for projects

---
+++ lib/data/integrity-checks/project.test.ts
@@ -0,0 +1,34 @@
+import prisma from "../../../prisma/client";
+import { describe, test, expect } from "@jest/globals";
+
+describe("Projects have one or more secondary departments", () => {
+  test("in 144 cases.", async () => {
+    const projects = await prisma.project.findMany({
+      include: {
+        departmentsSecondary: true,
+      },
+    });
+    const projectsWithSecondaryDepartment = projects.filter(
+      (d) => d.departmentsSecondary.length > 0
+    );
+
+    expect(projectsWithSecondaryDepartment.length).toBe(144);
+  });
+
+  test("which are all different from their main department.", async () => {
+    const projects = await prisma.project.findMany({
+      include: {
+        departmentsSecondary: true,
+      },
+    });
+    const projectsHaveRedundantSecondaryDepartments = projects.some(
+      (project) =>
+        project.departmentMainId &&
+        project.departmentsSecondary
+          .map((d) => d.id)
+          .includes(project.departmentMainId)
+    );
+
+    expect(projectsHaveRedundantSecondaryDepartments).toBe(false);
+  });
+});
