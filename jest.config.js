09156b137d20c4ec27f8b0cea2e3bd5ee53fbee2
jakoblistabarth <jakob@listabarth.at>
2023-08-07 09:05:28 +0200

    :wrench: Transform esm modules for jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Path to Next.js app to load next.config.js
+  dir: "./",
+});
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+const customJestConfig = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+};
+
+module.exports = async () => ({
+  ...(await createJestConfig(customJestConfig)()),
+  transformIgnorePatterns: ["node_modules/?!d3"],
+});
