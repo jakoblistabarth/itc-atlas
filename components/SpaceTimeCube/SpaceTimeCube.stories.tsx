561d128695b2dd8a4c9d3beed298b636a6cb174f
jakoblistabarth <jakob@listabarth.at>
2023-09-25 11:30:40 +0200

    :lipstick: Update lighting after bumbing three js to r152

--- components/SpaceTimeCube/SpaceTimeCube.stories.tsx
+++ components/SpaceTimeCube/SpaceTimeCube.stories.tsx
@@ -68,16 +68,16 @@ const meta = {
       return (
         <div style={{ width: "100%", height: "500px" }}>
           <Canvas
-            style={{ background: "black" }}
+            style={{ background: "white" }}
             orthographic
             camera={{ position: [0, 0, 100], zoom: 30 }}
             shadows
           >
-            <ambientLight args={[undefined, 0.1]} />
-            <hemisphereLight
-              color="#ffffff"
-              groundColor="#080820"
-              intensity={1.0}
+            <ambientLight args={[undefined, 2]} />
+            <directionalLight
+              position={[0, 5, 3]}
+              args={["white", 3]}
+              castShadow
             />
             <Story />
             <OrbitControls enablePan={false} />
