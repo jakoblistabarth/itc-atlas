0d0773b881f0d592e6e9f99adda6aadb0c55a570
Pei Nie <niepei@usc.edu.cn>
2023-11-01 18:28:02 +0800

    :sparkles: Introduce 3D map of travelsByCountryByDepartment

---
+++ lib/data/queries/btors/getBtorsGroupedByCountryByDepartment.ts
@@ -0,0 +1,29 @@
+import { group } from "d3";
+import prisma from "../../../../prisma/client";
+
+type QueryRow = {
+  country: string;
+  departmentId: string;
+  count: number;
+};
+
+export default async function getBtorsGroupedByCountryByDepartment() {
+  const rows: QueryRow[] = await prisma.$queryRaw`
+  SELECT
+	  "Country"."isoAlpha3" as "country",
+	  "Btor"."departmentId",
+	  CAST(COUNT(*) as INT)
+  FROM "Btor"
+  INNER JOIN "_BtorToCountry" ON "_BtorToCountry"."A" = "Btor".id
+  INNER JOIN "Country" ON "_BtorToCountry"."B" = "Country".id
+  GROUP BY "Btor"."departmentId", "Country"."isoAlpha3"
+  ORDER BY "Country"."isoAlpha3"
+  `;
+
+  const rowMap = group(rows, (d) => d.country);
+  return rowMap;
+}
+
+export type BtorsGroupedByCountryByDepartment = Awaited<
+  ReturnType<typeof getBtorsGroupedByCountryByDepartment>
+>;
