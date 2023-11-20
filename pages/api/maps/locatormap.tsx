9b00f7a9c157d48ade4c8d71066c448ff5e86ce4
jakoblistabarth <jakob@listabarth.at>
2023-11-20 10:54:31 +0100

    :bug: Make locatormap api route accept multiple countries

--- pages/api/maps/locatormap.tsx
+++ pages/api/maps/locatormap.tsx
@@ -18,6 +18,7 @@ import getCountries from "../../../lib/data/getCountries";
  *          schema:
  *            type: integer
  *          example: 400
+ *          default: 300
  *        - in: query
  *          name: country
  *          description: The country to highlight in the locator map.
@@ -39,17 +40,17 @@ import getCountries from "../../../lib/data/getCountries";
  */
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   const width = req.query.width ? Number(req.query.width) : undefined;
   if (Number.isNaN(width)) {
     return res.status(400).json({ error: "invalid width parameter" });
   }
-  const highlightCountries = req.query.country?.toString();
+  const highlightCountries = req.query.country;
   const [minLng, maxLat, maxLng, minLat] =
     req.query.bounds?.toString().split(",").map(Number) ?? [];
   if (!highlightCountries) {
-    res.status(400).json({ error: "invalid iso-3-code name" });
+    return res.status(400).json({ error: "invalid iso-3-code name" });
   }
   if (
     minLng < -180.0 &&
@@ -59,12 +60,14 @@ export default async function handler(
     minLng < maxLng &&
     maxLat > minLat
   ) {
-    res.status(400).json({
+    return res.status(400).json({
       error:
         "Please enter the coordinates in the following order: 'most southwesterly point, most northeasterly point' or 'minLng, maxLat, maxLng, minLat'",
     });
   }
-  const highlights: string[] = [highlightCountries ?? ""];
+  const highlights = Array.isArray(highlightCountries)
+    ? highlightCountries
+    : highlightCountries.split(",");
   const neCountriesTopoJson = getCountries();
   const svg = ReactDOMServer.renderToStaticMarkup(
     <LocatorMap
@@ -76,7 +79,7 @@ export default async function handler(
           bounds: [minLng, maxLat, maxLng, minLat],
         },
       ]}
-    ></LocatorMap>
+    ></LocatorMap>,
   );
 
   res
