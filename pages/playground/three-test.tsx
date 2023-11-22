10238bacddae9b972061cde6a7c824cb0f8cf19a
jakoblistabarth <jakob@listabarth.at>
2023-11-22 08:14:29 +0100

    :adhesive_bandage: Center objects in view

--- pages/playground/three-test.tsx
+++ pages/playground/three-test.tsx
@@ -1,4 +1,4 @@
-import { Box, Environment, OrbitControls } from "@react-three/drei";
+import { Bounds, Box, Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { range } from "d3";
 import type { NextPage } from "next";
@@ -27,32 +27,34 @@ const CanvasWithTooltip = () => {
     <>
       <Tooltip.Root open={isOpen} followCursor placement="bottom-start">
         <Tooltip.Trigger asChild>
-          <CanvasStage>
+          <CanvasStage className="h-[700px]">
             <Canvas
               orthographic
-              camera={{ position: [100, 100, 100], zoom: 100, near: 0 }}
+              camera={{ position: [100, 100, 100], near: 0 }}
             >
               <axesHelper />
               <Environment preset="apartment" />
-              {range(1000).map((d) => (
-                <Box
-                  key={d}
-                  onPointerEnter={(e) => {
-                    e.stopPropagation();
-                    setIsOpen(true);
-                    setHoverInfo(`id: ${d}`);
-                  }}
-                  onPointerOut={() => {
-                    setIsOpen(false);
-                    setHoverInfo(undefined);
-                  }}
-                  args={[1, 3, 1]}
-                  position={[(d % 50) * 2, 1.5, (d / 50) * 2]}
-                >
-                  <meshStandardMaterial color={"red"} />
-                </Box>
-              ))}
-              <OrbitControls target={[0, 5, 0]} />
+              <Bounds fit>
+                {range(1000).map((d) => (
+                  <Box
+                    key={d}
+                    onPointerEnter={(e) => {
+                      e.stopPropagation();
+                      setIsOpen(true);
+                      setHoverInfo(`id: ${d}`);
+                    }}
+                    onPointerOut={() => {
+                      setIsOpen(false);
+                      setHoverInfo(undefined);
+                    }}
+                    args={[1, 3, 1]}
+                    position={[(d % 50) * 2, 1.5, (d / 50) * 2]}
+                  >
+                    <meshStandardMaterial color={"red"} />
+                  </Box>
+                ))}
+              </Bounds>
+              <OrbitControls makeDefault />
               <Environment preset="apartment" />
             </Canvas>
           </CanvasStage>
