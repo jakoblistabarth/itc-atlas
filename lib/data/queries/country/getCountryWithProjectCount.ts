29b87f42cbd2d564a6680c9ee11535211b08dcc2
jakoblistabarth <jakob@listabarth.at>
2023-11-28 15:09:07 +0100

    :necktie: Only consider completed projects for count

--- lib/data/queries/country/getCountryWithProjectCount.ts
+++ lib/data/queries/country/getCountryWithProjectCount.ts
@@ -2,17 +2,27 @@ import { Prisma } from "@prisma/client";
 import prisma from "../../../../prisma/client";
 
 const getCountryWithProjectCount = async (departmentId?: string) => {
-  const filter = departmentId
-    ? {
-        where: { departmentMainId: departmentId },
-      }
-    : true;
+  const departmentFilter = departmentId
+    ? { departmentMainId: departmentId }
+    : undefined;
   return prisma.country.findMany({
     select: {
       isoAlpha3: true,
       unRegionCode: true,
       _count: {
-        select: { projects: filter },
+        select: {
+          projects: {
+            where: {
+              ...departmentFilter,
+              status: "Completed",
+            },
+          },
+        },
+      },
+    },
+    where: {
+      projects: {
+        some: { status: "Completed" },
       },
     },
   });
