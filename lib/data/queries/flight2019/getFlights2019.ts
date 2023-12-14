16bb48731a50c63537d60d6778a73bf1cfafe15d
Pei-Nie <122973203+Pei-Nie@users.noreply.github.com>
2023-12-14 18:13:07 +0800

    Update lib/data/queries/flight2019/getFlights2019.ts
    Co-authored-by: Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>

--- lib/data/queries/flight2019/getFlights2019.ts
+++ lib/data/queries/flight2019/getFlights2019.ts
@@ -1,13 +1,15 @@
 import prisma from "../../../../prisma/client";
 
-export default async function getFlights2019(...args: string[]) {
-  return args.length > 0
-    ? await prisma.flight2019.findMany({
-        where: {
-          departmentId: {
-            equals: args[0],
-          },
+export default async function getFlights2019(departmentId?: string) {
+  const filter = departmentId
+    ? {
+        departmentId: {
+          equals: departmentId,
         },
-      })
-    : await prisma.flight2019.findMany();
+      }
+    : undefined;
+
+  return await prisma.flight2019.findMany({
+    where: filter,
+  });
 }
