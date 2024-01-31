cf0fdec01bb3eca55b07835616b3ee535ad148c2
jakoblistabarth <jakob@listabarth.at>
2024-01-31 06:41:15 +0100

    :recycle: Refactor merge of historic and recent courses

--- lib/data/getCourseGenealogy.ts
+++ lib/data/getCourseGenealogy.ts
@@ -1,7 +1,6 @@
 import fs from "fs";
 import { CourseGenealogy } from "../../types/CourseGenealogy";
 import getMscApplicationsByProgramByYear from "./queries/application/getMscApplicationByProgramByYear";
-import { MscApplicationsByProgramByYearWithCount } from "./queries/application/getMscApplicationByProgramByYear";
 import { TimelineEvent } from "../../types/TimelineEvent";
 
 type ProgramStem = {
@@ -13,25 +12,27 @@ const getCourseGenealogy = async () => {
   const filePath = "./data/compiled/courseGenealogy.json";
   const raw = fs.readFileSync(filePath);
   const genealogy: CourseGenealogy = JSON.parse(raw.toString());
-  async function merge(programStem: ProgramStem[], years: number[]) {
-    // why using 'for' rather than the 'map' due to the multiple layer of the async function
-    for (let i = 0; i < programStem.length; i++) {
-      for (let j = 0; j < years.length; j++) {
-        const count: MscApplicationsByProgramByYearWithCount =
-          await getMscApplicationsByProgramByYear(
-            programStem[i].name,
-            years[j],
-          );
+
+  const mergeGenealogyData = async (
+    programStem: ProgramStem[],
+    years: number[],
+  ) => {
+    for (const programme of programStem) {
+      for (const year of years) {
+        const count = await getMscApplicationsByProgramByYear(
+          programme.name,
+          year,
+        );
         const course: TimelineEvent = {
-          name: programStem[i].name,
-          yOffset: programStem[i].name,
-          dateStart: new Date(years[j] + "-01-01T00:00:00.000Z"),
-          fill: programStem[i].stem,
+          name: programme.name,
+          yOffset: programme.name,
+          dateStart: new Date(year + ""),
+          fill: programme.stem,
           data: {
-            code: programStem[i].name,
-            year: years[j],
+            code: programme.name,
+            year: year,
             value: count[0]?._count._all,
-            stem: programStem[i].stem,
+            stem: programme.stem,
           },
           size: count[0]?._count._all,
         };
@@ -39,9 +40,9 @@ const getCourseGenealogy = async () => {
       }
     }
     return genealogy;
-  }
+  };
 
-  return await merge(
+  return await mergeGenealogyData(
     [
       { name: "GFM", stem: "1" },
       { name: "NRM", stem: "2" },
